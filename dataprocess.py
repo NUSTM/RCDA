302222e4bb72daab37f46dbc358a26dc2b554160
Hao Chen <37237656+Jcorners@users.noreply.github.com>
2022-03-04 09:37:26 +0800

    Update dataprocess.py

--- dataprocess.py
+++ dataprocess.py
@@ -4,39 +4,44 @@ from util import get_new_sentence
 from torch.utils.data import Dataset
 
 
-def get_rev_txtid(new_sentence, w2id, max_seq):
+def get_rev_txtid(new_sentence, w2id, max_seq,k):
 
     ant_txtid=[[] for _ in range(max_seq)]
     if len(new_sentence) > max_seq:
         new_sentence = new_sentence[:max_seq]
     while len(new_sentence) < max_seq:
         new_sentence.append(['[PAD]'])
+        
     for index,word_lis in enumerate(new_sentence):
         for token in word_lis:
             if token in w2id:
                 ant_txtid[index].append(w2id[token])
             else:
                 ant_txtid[index].append(w2id['[UNK]'])
-        while (len(ant_txtid[index]) < 1):
+        while (len(ant_txtid[index]) < k):
             ant_txtid[index].append(w2id['[MUL]'])
 
     return ant_txtid
 
 def get_raw_txtid(txt,w2id,max_seq):
     txtid= []
+
     for i in (txt.split()):
         if i in w2id:
             txtid.append(w2id[i])
         else:
             txtid.append(w2id['[UNK]'])
+
     while(len(txtid)<max_seq):
         txtid.append(w2id['[PAD]'])
     if (len(txtid)>max_seq):
         txtid=txtid[:max_seq]
+
     return txtid
 
-def get_data(path, w2id, ant_word_dic,max_seq):
+def get_data(path, w2id, ant_word_dic,max_seq,k):
     src_text,rev_text, labels = [], [], []
+
     with open(path, encoding='utf-8') as f:
         for line in tqdm(f):
             line = line.strip()
@@ -46,14 +51,14 @@ def get_data(path, w2id, ant_word_dic,max_seq):
             raw_text_id=get_raw_txtid(raw_text,w2id,max_seq)
             src_text.append(raw_text_id)
             new_sentence = get_new_sentence(raw_text, ant_word_dic)  # list:[[word],[word0，word1]...]
-            rev_text_id = get_rev_txtid(new_sentence, w2id, max_seq)
+            rev_text_id = get_rev_txtid(new_sentence, w2id, max_seq,k)
             rev_text.append(rev_text_id)
     return src_text, rev_text, labels
 
 
 class textDataset_genetate(Dataset):
-    def __init__(self, path, w2id, ant_word_dic,max_seq):
-        src_text, tgt_text, label = get_data(path, w2id,ant_word_dic,max_seq)
+    def __init__(self, path, w2id, ant_word_dic,max_seq,k):
+        src_text, tgt_text, label = get_data(path, w2id,ant_word_dic,max_seq,k)
         self.src = torch.tensor(src_text)  # batch seq
         self.tgt = torch.tensor(tgt_text)
         self.label = torch.tensor(label)
